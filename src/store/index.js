230ea1a3cf073cd4c92f98e763a83c3216d092ba
Henry-Kurniawan <henryksutanto@gmail.com>
2021-09-22 11:10:43 +0700

    Add: User Login, Logout, and router link related to it

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,43 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    isLoggedIn: false,
+    baseURL: 'http://localhost:4002',
+    logged_email: ""
+  },
+  mutations: {
+    SET_LOGIN(state, data) {
+      localStorage.setItem("access_token", data.access_token)
+      localStorage.setItem("id", data.id)
+      localStorage.setItem("trelloListId", data.trelloListId)
+      localStorage.setItem("email", data.email)
+      state.isLoggedIn = true
+      state.logged_email = localStorage.getItem('email')
+    },
+
+    SET_ALREADY_LOGIN(state) {
+      state.isLoggedIn = true
+      state.logged_email = localStorage.getItem('email')
+    },
+
+    SET_LOGOUT(state) {
+      localStorage.clear()
+      state.isLoggedIn = false
+    },
+  },
+  actions: {
+    actionLoginHandler(context, login_payload) {
+      return axios({
+        url: `${this.state.baseURL}/users/login`,
+        method: "post",
+        data: login_payload
+      })
+    },
+  },
   modules: {},
 });
